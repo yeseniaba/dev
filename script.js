eb1873f3480a71a162be2dcd26151f22ce38e0f9
yeseniaba <73720916+yeseniaba@users.noreply.github.com>
2021-09-17 14:40:01 -0700

    update

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
-
-var setTheme = function (theme) {
-if (theme === 'dark') {
-// dark
-$( "body" ).removeClass("standard");
-$( "body" ).addClass( "dark" );
-$(".inner-switch").text("ON");
-setCookie('Theme', 'dark', 30);
-} else {
-$("body").removeClass("dark");
-$("body").addClass("standard");
-$(".inner-switch").text("OFF");
-setCookie('Theme', 'standard', 30);
-}
-};
-
-currentTheme = getCookie('Theme');
-setTheme(currentTheme);
+// Creating dark mode toggle
 
 $( ".inner-switch" ).on("click", function() {
-if ($("body").hasClass("dark")) {
-// standard
-setTheme('standard');
-} else {
-// dark mode
-setTheme('dark');
-}
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "OFF" );
+    } else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "ON" );
+    }
 });
