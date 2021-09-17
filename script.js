ab96baecd2773be73796fd00ed85d9f3bae9372d
yeseniaba <73720916+yeseniaba@users.noreply.github.com>
2021-09-17 14:37:43 -0700

    update

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-// Creating dark or light mode toggle 
 
-$( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "ON" );
-    }
-});
-
-// Persisting dark or light mode
-
-// dark mode switch
 var setTheme = function (theme) {
 if (theme === 'dark') {
 // dark
