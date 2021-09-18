bec9ba5c1eb84fc32cf04af3f9f58b7c9291dc01
yeseniaba <73720916+yeseniaba@users.noreply.github.com>
2021-09-17 20:20:08 -0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 // Creating dark mode toggle
 
-$( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark" )) {
-      $( "body" ).removeClass( "dark" );
-      $( ".inner-switch" ).text( "OFF" );
-    } else {
-      $( "body" ).addClass( "dark" );
-      $( ".inner-switch" ).text( "ON" );
+const switchButton = document.getElementById('switch');
+ 
+switchButton.addEventListener('click', () => {
+document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+switchButton.classList.toggle('active');
+//toggle the HTML button with the id='switch' with the class 'active'
+});
+
+// Creating local storage
+
+const switchButton = document.getElementById('switch');
+const workContainer = document.getElementById('work');
+ 
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); //toggle the HTML body the class 'dark'
+    switchButton.classList.toggle('active');//toggle the HTML button with the id='switch' with the class 'active''
+    workContainer.classList.toggle('dark');
+ 
+   if(document.body.classList.contains('dark')){ //when the body has the class 'dark' currently
+        localStorage.setItem('darkMode', 'enabled'); //store this data if dark mode is on
+    }else{
+        localStorage.setItem('darkMode', 'disabled'); //store this data if dark mode is off
     }
 });
